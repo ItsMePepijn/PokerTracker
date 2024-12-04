1b47357007cd2d79cc6123578a7a27257b5199bc
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2024-12-04 22:49:39 +0100

    ensure database always exists

---
+++ PokerTracker.Persistence/Seeder.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PokerTracker.Persistence
+{
+	public static class Seeder
+	{
+		public static void EnsureDatabaseExists(this IServiceProvider serviceProvider)
+		{
+			using var scope = serviceProvider.CreateScope();
+			var contextFactory = scope.ServiceProvider.GetRequiredService<Func<DataContext>>();
+
+			var context = contextFactory();
+
+			context.Database.Migrate();
+		}
+	}
+}
--- PokerTracker.Service/Program.cs
+++ PokerTracker.Service/Program.cs
@@ -29,6 +29,8 @@ if (app.Environment.IsDevelopment())
 	app.MapOpenApi();
 }
 
+app.Services.EnsureDatabaseExists();
+
 app.UseAuthorization();
 
 app.MapControllers();
